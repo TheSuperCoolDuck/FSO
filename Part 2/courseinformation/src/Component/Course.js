41544060cfd7217333701a5e5a8c15bc61a7e1c8
TheSuperCoolDuck <Dchen0058@gmail.com>
2021-11-29 17:18:42 +1100

    Exercise 2.5

--- Part 2/courseinformation/src/Component/Course.js
+++ Part 2/courseinformation/src/Component/Course.js
@@ -3,16 +3,14 @@ import React from 'react'
 const Course=({course})=>{
     return(
         <div>
-            <h1>{course.name}</h1> 
+            <h2>{course.name}</h2> 
             {course.parts.map(part=>
                 <p key={part.id}>
                     {part.name} {part.exercises}
                 </p>
             )}   
             <b>
-                total of {course.parts.reduce( (total, part) => {
-                    return total + part.exercises
-                }, 0)} exercises
+                total of {course.parts.reduce( (total, part) => total + part.exercises, 0)} exercises
             </b>
         </div>
     )
