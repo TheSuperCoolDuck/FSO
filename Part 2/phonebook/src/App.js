f61dfcc8130da551d0a0cdc80c3d8f3c9845d4aa
TheSuperCoolDuck <Dchen0058@gmail.com>
2021-11-29 20:30:40 +1100

    Exercise 2.8

--- Part 2/phonebook/src/App.js
+++ Part 2/phonebook/src/App.js
@@ -2,9 +2,11 @@ import React, {useState} from 'react'
 
 const App = ()=> {
   const[persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    {name: 'Arto Hellas',
+     number: '040-1234567'}
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const personIsInArray = (personsArray, person)=>{
     if(personsArray.find(p=>p.name===person.name)){
@@ -17,7 +19,8 @@ const App = ()=> {
     event.preventDefault();
     
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     if(personIsInArray(persons,personObject)){
@@ -27,10 +30,14 @@ const App = ()=> {
     }
 
     setNewName('')
+    setNewNumber('')
   }
 
-  const handleNoteChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) =>{
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -38,14 +45,17 @@ const App = ()=> {
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNoteChange}/>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person=><div key={person.name}>{person.name}</div>)}
+      {persons.map(person=><div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 
