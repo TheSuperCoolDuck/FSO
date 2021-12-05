112d63f71cdc995d34f1918f544184bb92f731af
TheSuperCoolDuck <Dchen0058@gmail.com>
2021-12-05 21:40:51 +1100

    Exercise 3.8

--- Part 3/phonebook_backend/index.js
+++ Part 3/phonebook_backend/index.js
@@ -1,8 +1,13 @@
 const { response } = require('express')
 const express = require('express')
+const morgan = require('morgan')
+
+morgan.token('body', (request, response) => JSON.stringify(request.body));
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
